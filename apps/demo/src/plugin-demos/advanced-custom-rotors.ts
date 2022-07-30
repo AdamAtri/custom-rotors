5d91f495db3f644c2045a92e626edc1b5c56473f
Adam Atri <adam@atricoware.com>
2022-07-29 21:55:53 -0400

    updates advanced uses example

--- apps/demo/src/plugin-demos/advanced-custom-rotors.ts
+++ apps/demo/src/plugin-demos/advanced-custom-rotors.ts
@@ -1,7 +1,12 @@
-import { Page, NavigatedData, GridLayout } from '@nativescript/core';
+import { Page, NavigatedData, GridLayout, View, ContentView, LayoutBase } from '@nativescript/core';
 
 export function navigatingTo(d: NavigatedData): void {
-  (<Page>d.object).rotorGroupCallbacks.set('rating', ({ forward }) => {
+  const page = d.object as Page;
+  page.rotorGroupCallbacks.set('group1', highlightSelected.bind(null, page));
+  page.rotorGroupCallbacks.set('group2', highlightSelected.bind(null, page));
+  page.rotorGroupCallbacks.set('group3', highlightSelected.bind(null, page));
+  page.rotorGroupCallbacks.set('rating', ({ forward }) => {
+    clearSelected(page);
     console.log('rating rotorgroup callback ', forward, 'hmm');
     incrementOrDecrementRating(d.object as any, forward);
   });
@@ -15,4 +20,17 @@ function incrementOrDecrementRating(page: Page, increment: boolean): void {
     const view = ratingBar.getChildAt(i);
     view.backgroundColor = i < rating ? 'green' : 'transparent';
   }
+}
+
+function clearSelected(view: View): boolean {
+  if (view instanceof ContentView) clearSelected(view.content);
+  else if (view instanceof LayoutBase) {
+    view.eachChildView((v) => clearSelected(v));
+  } else view.deletePseudoClass('selected');
+  return true;
+}
+
+function highlightSelected(page: Page, { newView }): void {
+  clearSelected(page);
+  (<View>newView).addPseudoClass('selected');
 }
